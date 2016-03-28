429dd71898f32bd461abec04447e5d2708ca40bf
Ilya Bohaslauchyk <3751950@gmail.com>
2016-03-28 13:08:56 +0300

    Replaced all objects for storing data to sets

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by Phaeton on 26.03.2016.
  */
- 
+
 //The tree contains every letter from the text only once
 //Every letter contains indecies of its every parent (every previous letter in the text) and every child (every next letter in the text)
 function Tree (text) {
@@ -10,31 +10,32 @@ function Tree (text) {
         previous,
         previousIndex;
 
+	//get every letter from the text
     for (var i = 0; i < text.length; i++) {
 
         current = text.charAt(i);
-        //if there is no such letter in the tree, create a new node for this letter and store its index
+		//if there is no such letter in the tree, create a new node for this letter and store its index
         if (this[current] === undefined) {
             this[current] = {
-                indecies:{},
-                children:{},
-                parents:{}
+                indecies: new Set (),
+                children: {},
+                parents: {}
             };
-            this[current].indecies[i] = null;
+            this[current].indecies.add (i); 
         }
-        //if there is such letter in the tree, only store its index in the existing node
+		//if there is such letter in the tree, only store its index in the existing node
         else {
-            this[current].indecies[i] = null;
+            this[current].indecies.add (i);
         };
 
-        //if there is a parent, store its index
+		//if there is a parent, store its index
 		//inside the parent store the index of the current letter among children
         if (previous) {
-            if (!this[current].parents[previous]) this[current].parents[previous] = {};
-            if (!this[previous].children[current]) this[previous].children[current] = {};
+            if (!this[current].parents[previous]) this[current].parents[previous] = new Set ();
+            if (!this[previous].children[current]) this[previous].children[current] = new Set ();
 
-            this[current].parents[previous][i] = null;
-            this[previous].children[current][previousIndex] = null;
+            this[current].parents[previous].add (i);
+            this[previous].children[current].add (previousIndex);
         };
         previous = current;
         previousIndex = i;
@@ -46,25 +47,25 @@ function Tree (text) {
 Tree.prototype.search = function (str) {
     if (str.length === 1) return this[str].indecies;
 
-    var result = {};
+    var result = new Set ();
 
-    //set result with all indecies of the first letter
+	//set result with all indecies of the first letter
     var currentSymbol = str.charAt(0),
         nextSymbol = str.charAt(1),
         previousSymbol = currentSymbol;
     if (!this[currentSymbol] || !this[currentSymbol].children[nextSymbol]) return null;
-    for (var index in this[currentSymbol].children[nextSymbol]) {
-        result[index] = null;
+    for (var index of this[currentSymbol].children[nextSymbol]) {
+        result.add (index);
     };
-    
-    //go from parent to child, if there is no next child, delete the corresponding property from result
+
+	//go from parent to child, if there is no next child, delete the corresponding property from result
     for (var i = 1; i < str.length - 1; i++) {
         currentSymbol = str.charAt(i),
         nextSymbol = str.charAt(i + 1);
         if (!this[currentSymbol] || !this[currentSymbol].children[nextSymbol]) return null;
 
-        for (var index in this[currentSymbol].parents[previousSymbol]) {
-            if (this[currentSymbol].children[nextSymbol][index] === undefined) delete result[index - i];
+        for (var index of this[currentSymbol].parents[previousSymbol]) {
+            if (!this[currentSymbol].children[nextSymbol].has(index)) result.delete(index - i);
         };
 
         previousSymbol = currentSymbol;
@@ -75,4 +76,8 @@ Tree.prototype.search = function (str) {
 
 var p = document.querySelector("p");
 var tree = new Tree(p.textContent);
-var matches = tree.search("ear");
+var matches = tree.search("the");
+
+for (var i of matches) {
+	alert (i);
+};
