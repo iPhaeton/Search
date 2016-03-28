5f4f0425a7a097183680532c5e902c86d4041273
Ilya Bohaslauchyk <3751950@gmail.com>
2016-03-28 11:50:31 +0300

    Add comments to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 /**
  * Created by Phaeton on 26.03.2016.
  */
-
+ 
+//The tree contains every letter from the text only once
+//Every letter contains indecies of its every parent (every previous letter in the text) and every child (every next letter in the text)
 function Tree (text) {
 
     var current,
@@ -11,6 +13,7 @@ function Tree (text) {
     for (var i = 0; i < text.length; i++) {
 
         current = text.charAt(i);
+        //if there is no such letter in the tree, create a new node for this letter and store its index
         if (this[current] === undefined) {
             this[current] = {
                 indecies:{},
@@ -19,10 +22,13 @@ function Tree (text) {
             };
             this[current].indecies[i] = null;
         }
+        //if there is such letter in the tree, only store its index in the existing node
         else {
             this[current].indecies[i] = null;
         };
 
+        //if there is a parent, store its index
+		//inside the parent store the index of the current letter among children
         if (previous) {
             if (!this[current].parents[previous]) this[current].parents[previous] = {};
             if (!this[previous].children[current]) this[previous].children[current] = {};
@@ -42,6 +48,7 @@ Tree.prototype.search = function (str) {
 
     var result = {};
 
+    //set result with all indecies of the first letter
     var currentSymbol = str.charAt(0),
         nextSymbol = str.charAt(1),
         previousSymbol = currentSymbol;
@@ -49,7 +56,8 @@ Tree.prototype.search = function (str) {
     for (var index in this[currentSymbol].children[nextSymbol]) {
         result[index] = null;
     };
-
+    
+    //go from parent to child, if there is no next child, delete the corresponding property from result
     for (var i = 1; i < str.length - 1; i++) {
         currentSymbol = str.charAt(i),
         nextSymbol = str.charAt(i + 1);
