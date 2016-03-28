c6cf8fe8206a7f5f752bf170a2fef46c391b7fdf
Ilya Bohaslauchyk <3751950@gmail.com>
2016-03-28 19:10:09 +0300

    Add behaviour to the search panel

---
+++ scripts/events.js
@@ -0,0 +1,52 @@
+function documentKeyDown (event) {
+	if (event.ctrlKey && event.keyCode === 70) {
+		showSearchPanel ();
+		event.preventDefault ();
+	};
+};
+
+//Prevent search by default
+function documentKeyPress (event) {
+	var char = getChar(event);
+	if (event.ctrlKey && (char === "f" || char === "F")) {
+		event.preventDefault ();
+	};
+};
+
+function showSearchPanel () {
+	var searchPanel = document.getElementById ("search-panel");
+	
+	//show search panel
+	if (searchPanel.hidden) {
+		searchPanel.hidden = "";
+	
+		//to the beginning of the document insert a div a panel height to scroll the document down
+		var fakePanel = document.createElement("div");
+		fakePanel.id = "fake-panel"
+		fakePanel.style.height = searchPanel.offsetHeight + "px";
+		fakePanel.style.display = "block";
+		document.body.insertBefore (fakePanel, document.body.firstChild);
+	}
+	//hide search panel
+	else {
+		searchPanel.hidden = "true";
+		
+		//remove the fake div
+		var fakePanel = document.getElementById ("fake-panel");
+		document.body.removeChild (fakePanel);
+	};
+};
+
+function getChar(event) {
+  if (event.which == null) { // IE
+    if (event.keyCode < 32) return null; // спец. символ
+    return String.fromCharCode(event.keyCode)
+  };
+
+  if (event.which != 0 && event.charCode != 0) { // все кроме IE
+    if (event.which < 32) return null; // спец. символ
+    return String.fromCharCode(event.which); // остальные
+  };
+
+  return null; // спец. символ
+};
--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,10 @@
  * Created by Phaeton on 26.03.2016.
  */
 
-
+document.addEventListener ("keydown", documentKeyDown);
+document.addEventListener ("keypress", documentKeyPress);
 
 var p = document.querySelector("p");
 var tree = new Tree(p);
-var matches = tree.search("ear");
+var matches = tree.search("dest");
 tree.select (matches);
-
-
