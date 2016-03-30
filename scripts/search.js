eeea2c0e01463732b3ed6de5aadee1a44e12e531
Ilya Bohaslauchyk <3751950@gmail.com>
2016-03-30 17:25:45 +0300

    Add sequential selection
    Added step by step sequential selection for sequential search

--- scripts/search.js
+++ scripts/search.js
@@ -107,64 +107,13 @@ Tree.prototype.sequentialSearch = function (symbol) {
     return true;
 };
 
-/*Tree.prototype.search = function (str) {
-	var start = this.found.length;
-	this.found += str;
-	
-	//set initial found positions with all indecies of the first letter
-	if (!this.foundPositions.size) {
-		var currentSymbol = this.found.charAt(0),
-			nextSymbol = this.found.charAt(1),
-			previousSymbol = currentSymbol;
-		
-		if (!this[currentSymbol]) return false;
-		
-		if (nextSymbol) {
-			if (!this[currentSymbol].children[nextSymbol]) return false;
-			for (var index of this[currentSymbol].children[nextSymbol]) {
-				this.foundPositions.add (index);
-			};
-		}
-		else {
-			for (var index of this[currentSymbol].indecies) {
-				this.foundPositions.add (index);
-			};
-			return true;
-		};
-	};
-
-    //go from parent to child, if there is no next child, delete the corresponding property from found positions
-	for (var i = start; i < this.found.length; i++) {
-		var currentSymbol = this.found.charAt(i),
-        var previousSymbol = this.found.charAt(i - 1);
-		
-		if (!this[currentSymbol].parents[previousSymbol]) return false;
-		
-		for (var index of this[currentSymbol].parents[previousSymbol]) {
-			
-		};
-	};
-	
-    /*for (var i = start; i < this.found.length - 1; i++) {
-        currentSymbol = this.found.charAt(i),
-        nextSymbol = this.found.charAt(i + 1);
-        if (!this[currentSymbol] || !this[currentSymbol].children[nextSymbol]) return false;
-
-        for (var index of this[currentSymbol].parents[previousSymbol]) {
-            if (!this[currentSymbol].children[nextSymbol].has(index)) this.foundPositions.delete(index - i);
-        };
-
-        previousSymbol = currentSymbol;
-    };
-};*/
-
 //Selection of found matches after search
 Tree.prototype.select = function (selectionStyle) {
 	var points = this.foundPositions,
 		offset = this.found.length,
 		previousStartPoint = 0;
 
-	var j = 0; //when a span is added to a text node, text is being divided, so I need to cout the amount of nodes
+	var j = 0; //when a span is added to a text node, text is being divided, so I need to count the amount of nodes
 		
 	for (var startPoint of points) {
 		var range = document.createRange ();
@@ -184,6 +133,7 @@ Tree.prototype.select = function (selectionStyle) {
 		var highLight = document.createElement ("span");
 		highLight.classList.add ("highlight");
 		highLight.style.cssText = selectionStyle;
+		highLight.dataset.position = startPoint;
 		
 		range.surroundContents (highLight);
 		
@@ -192,7 +142,7 @@ Tree.prototype.select = function (selectionStyle) {
 	};
 };
 
-Tree.prototype.deselect = function () {
+Tree.prototype.deselectAll = function () {
 	//to keep text content of parent element in one node,
 	//I delete all nodes from the element and replace them
 	//with one text node which contains whole text,
@@ -206,9 +156,44 @@ Tree.prototype.deselect = function () {
 	this.parentElem.textContent = content;
 };
 
+Tree.prototype.sequentialSelect = function () {
+	var highLights = document.getElementsByClassName ("highlight");
+	
+	for (var i = 0; i < highLights.length; i++) {
+		
+		if (!this.foundPositions.has (+highLights[i].dataset.position)) {
+			this.deseleteSpan (highLights[i--]);
+		}
+		else {
+			if (highLights[i].nextSibling.textContent) var nextSibling = highLights[i].nextSibling;
+			else var nextSibling = highLights[i].nextSibling.nextSibling;
+			
+			highLights[i].textContent += nextSibling.textContent.charAt(0); //add next symbol to highlight
+			nextSibling.textContent = nextSibling.textContent.slice (1); //remove next symbol from the next sibling
+		};
+		
+	};
+};
+//????????????????????????????????????????????????????????????????????????????????
+Tree.prototype.deseleteSpan = function (span) {
+	var content = "";
+	
+	//gather content 
+	if (span.previousSibling.nodeType === 3) content += span.previousSibling.data;
+	content += span.textContent;
+	if (span.nextSibling.nodeType === 3) content += span.nextSibling.data;
+	
+	//replace nodes with a new new node with the same content
+	var newTextNode = document.createTextNode (content);
+	if (span.previousSibling.nodeType === 3) this.parentElem.removeChild (span.previousSibling);
+	if (span.nextSibling.nodeType === 3) this.parentElem.removeChild (span.nextSibling);
+	this.parentElem.insertBefore (newTextNode, span);
+	this.parentElem.removeChild (span);
+};
+//????????????????????????????????????????????????????????????????????????????????
 //Clear the results of the last search
 Tree.prototype.clear = function () {
-	this.deselect ();
+	this.deselectAll ();
 	this.found = "";
 	this.foundPositions.clear ();
 }
